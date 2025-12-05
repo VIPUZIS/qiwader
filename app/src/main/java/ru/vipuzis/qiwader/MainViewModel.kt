88a955912dd02216cfd59faca1dc624c83629aac
student <japanesecherry0511@gmail.com>
2025-12-05 18:12:30 +0300

    QiwaderRU beta 0.2.02

--- app/src/main/java/ru/vipuzis/qiwader/MainViewModel.kt
+++ app/src/main/java/ru/vipuzis/qiwader/MainViewModel.kt
@@ -1,15 +1,27 @@
 package ru.vipuzis.qiwader
 
+import android.app.Application
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.core.content.edit
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 
-class MainViewModel: ViewModel() {
-    val vm = MainViewModel()
-    var count = 0
-    var multiple = 1
-    val isUsedBoostClicks = 0
-    val isUsedBoostAutoclicker = 0
-    val isUsedBoostPassiveIncome = 0
-    fun Plus(multiple: Int): Int {
-        return 1*multiple
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    private val preferences = application.getSharedPreferences("main", Context.MODE_PRIVATE)
+    private val _count = MutableLiveData<Int>()
+    val count: LiveData<Int> = _count
+
+    init {
+        val saved = preferences.getInt("count", 0)
+        _count.value = saved
+    }
+
+    fun plus() {
+        val newValue = (_count.value ?: 0) + 1
+        _count.value = newValue
+        preferences.edit { putInt("count", newValue) }
     }
 }
